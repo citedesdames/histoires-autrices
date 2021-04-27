f59d0095cb0a453d992c93f5311cc2c6ab52c92b
dongjounkim <dongjounkim@gmail.com>
2021-04-27 17:01:38 +0200

    Show female authors, tweaking with chart.js and more CSS

--- assets/script.js
+++ assets/script.js
@@ -55,23 +55,36 @@ $.get('/assets/data/data1.csv', function (csvString) {
         },
         dynamicTyping: true
     }).data;
-    // console.log(data1[0].Genre);
 
     var size = data1.length;
-    console.log("Size :".size);
     let autrice = 0;
     let auteur = 0; 
+    let autrice_noms = [];
 
+    console.log(data1);
+
+    //Count the number of female and male authors appearing in French 'collège' school programmes
     for (var i = 0; i < size; i++) {
-        if (data1[i].Genre == "F") {
+        if (data1[i].Genre == "F" && data1[i]['Jeu de données'] == "Programmes du collège" ) {
             autrice++;
-        } else if (data1[i].Genre == "M") {
+            autrice_noms.push(data1[i]["Auteur ou autrice"]);
+        } else if (data1[i].Genre == "M" && data1[i]['Jeu de données'] == "Programmes du collège" ) {
             auteur++;
         }       
     }
-    
     console.log(autrice);
     console.log(auteur);
+    console.log(autrice_noms);
+
+    let autriceLinks = [];
+
+    //Find and show links to female authors
+    for (var i = 0; i < autrice_noms.length; i++){
+        autriceLinks.push(`<a class="data__chart__text__link" href="#"> ${autrice_noms[i]}</a>`);
+    }
+    document.getElementById("autrice_nom").innerHTML = autriceLinks;
+
+    //data for pie chart
     data = {
         datasets: [{
             data: [ auteur, autrice],
@@ -86,24 +99,38 @@ $.get('/assets/data/data1.csv', function (csvString) {
 
     };
 
-    var ctx = document.getElementById("mychart");
+    //Generate a pie chart with chart.js
+    var ctx = document.getElementById("mychart").getContext('2d');
     var chart = new Chart(ctx, {
         type: 'pie',
         data: data,
         options: {
-        responsive: true,
-        plugins: {
-            legend: {
-            position: 'top',
-            },
-            title: {
-            display: true,
-            text: 'Chart.js Pie Chart'
-            },
-            datasource: {
-                url: '/assets/data/data1.csv'
+            responsive: true,
+            plugins: {
+                legend: {
+                position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Chart.js Pie Chart'
+                },
+                datasource: {
+                    url: '/assets/data/data1.csv'
+                },
+                datalabels: {
+                    formatter: (value, ctx) => {
+                        let sum = 0;
+                        let dataArr = ctx.chart.data.datasets[0].data;
+                        dataArr.map(data => {
+                            sum += data;
+                        });
+                        let percentage = (value*100 / sum).toFixed(2)+"%";
+                        return percentage;
+                    },
+                    color: '#000',
+                    font: { weight: 'bold', size: 24},
+                }
             }
-        }
         }
     });
 
